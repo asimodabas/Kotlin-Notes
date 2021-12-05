63300bfb731b1ad1cdc1b145e44de82550f3bab9
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-12-05 20:44:00 +0300

    Kotlin notes added

--- app/src/main/java/com/asimodabas/kotlingiris/MainActivity.kt
+++ app/src/main/java/com/asimodabas/kotlingiris/MainActivity.kt
@@ -7,5 +7,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //Lambda expressions
+
+        fun printString(myString : String) {
+            println(myString)
+        }
+
+        printString("My Test String")
+
+        val testString = {myString : String -> println(myString)}
+
+        testString("My Lambda String")
+
+        val multiplyLambda = {a: Int, b: Int -> a * b }
+        println(multiplyLambda(5,4))
+
+        val multiplyLambda2 : (Int, Int) -> Int = {a,b -> a*b}
+        println(multiplyLambda2(5,5))
+
     }
+
 }
