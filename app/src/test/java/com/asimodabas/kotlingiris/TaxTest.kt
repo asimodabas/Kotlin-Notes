6ab9633b5ea0b572d19d7f0a9dacc379b8639fed
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-02-24 13:35:40 +0300

    Testing

--- app/src/test/java/com/asimodabas/kotlingiris/TaxTest.kt
+++ app/src/test/java/com/asimodabas/kotlingiris/TaxTest.kt
@@ -2,14 +2,30 @@ package com.asimodabas.kotlingiris
 
 import com.google.common.truth.Truth.assertThat
 import junit.framework.TestCase
+import org.junit.After
+import org.junit.Before
 
 class TaxTest : TestCase() {
 
+    private lateinit var tax : Tax
+
+    @Before
+    fun setup(){
+        tax = Tax()
+    }
+
+    @After
+    fun teardown(){
+
+    }
+
     fun testCalculateTax() {
-        val tax = Tax()
         val netTax = tax.calculateTax(100.0,0.1)
         assertThat(netTax).isEqualTo(10)
     }
 
-    fun testCalculateIncome() {}
+    fun testCalculateIncome() {
+        val netIncome=tax.calculateIncome(200.0,0.2)
+        assertThat(netIncome).isEqualTo(160)
+    }
 }
